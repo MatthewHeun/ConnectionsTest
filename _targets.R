52c4bee22c3566b54270a5d55fdf4706a21eb492
Matthew Heun <matthew.heun@me.com>
2024-07-19 14:08:12 -0400

    New info on parallel targets

--- _targets.R
+++ _targets.R
@@ -9,7 +9,7 @@ library(targets)
 
 # Set target options:
 tar_option_set(
-  packages = c("tibble") # Packages that your targets need for their tasks.
+  packages = c("tibble"), # Packages that your targets need for their tasks.
   # format = "qs", # Optionally set the default storage format. qs is fast.
   #
   # Pipelines that take a long time to run may benefit from
@@ -21,7 +21,7 @@ tar_option_set(
   # which run as local R processes. Each worker launches when there is work
   # to do and exits if 60 seconds pass with no tasks to run.
   #
-  #   controller = crew::crew_controller_local(workers = 2, seconds_idle = 60)
+  controller = crew::crew_controller_local(workers = 2, seconds_idle = 60)
   #
   # Alternatively, if you want workers to run on a high-performance computing
   # cluster, select a controller from the {crew.cluster} package.
@@ -47,6 +47,53 @@ tar_option_set(
 # Run the R scripts in the R/ folder with your custom functions:
 # tar_source()
 # tar_source("other_functions.R") # Source other scripts as needed.
+
+
+# # Run with the following in the RStudio console:
+# parallelly::availableConnections()
+# # The answer is 128.
+# targets::tar_make() # To run the targets pipeline.
+# # Check the answer
+# targets::tar_read(conns) # To read the answer.
+# # At the RStudio console, the answer is 128.
+
+# # Try from the command line in Terminal
+# # Start a new R process with 512 connections.
+# mkh2@Mac56347 ConnectionsTest % R --max-connections=512
+# # Now type
+# parallelly::availableConnections()
+# # The answer is 512, as expected.
+# # Now type
+# targets::tar_destroy() # To destroy the targets cache.
+# targets::tar_make()
+# targets::tar_read(conns)
+# # The answer should be 512, but I get 128.
+# # That's because targets starts a new process 
+# # in which it runs the pipeline.
+
+# # To get around the new process issue, we can 
+# # invoke tar_make() in another way.
+# # See https://docs.ropensci.org/targets/reference/tar_make.html
+# # for a discussion that says 
+# # "Set [callr_function] to NULL 
+# #  to run in the current session instead of an external process".
+# targets::tar_destroy()
+# targets::tar_make(callr_function = NULL)
+# targets::tar_read(conns)
+# # Now, I get 512, as expected.
+
+# # Try to run in parallel mode by uncommenting the "controller = " line above.
+# # targets should create 2 "runners" and execute the pipeline 
+# # on one of the runners.
+# targets::tar_destroy()
+# targets::tar_make(callr_function = NULL)
+# targets::tar_read(conns)
+# # Answer is 128.
+# So it appears that the new (parallel) processes
+# do not inherit the number of connections from 
+# the parent process.
+# Instead, the new (parallel) processes 
+# use the default 128 connections.
 
 # Replace the target list below with your own:
 list(
